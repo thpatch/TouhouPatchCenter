640183f9b27e9cd0eb5cfb72f0de76649a72f0b2
nmlgc <nmlgc@thpatch.net>
2013-07-22 03:00:44 +0200

    TPCFmtSpells.php: Don't escape spell cards after all.
    We needed to do this because spell names are actually sprintf format strings, even though the games never actually take advantage of this. Thus, it should be our goal to *remove* these unnecessary sprintf calls *in Touhou code* and not to work around them, as they have been always vulnerable to buffer overflows.

--- hooks/TPCFmtSpells.php
+++ hooks/TPCFmtSpells.php
@@ -24,7 +24,7 @@ class TPCFmtSpells {
 		
 		$spells = &$tpcState->switchDataFile( "spells.js" );
 		if ( $name ) {
-			$spells[$id] = TPCUtil::sanitize( $name );
+			$spells[$id] = $name;
 		}
 
 		// Comments...
