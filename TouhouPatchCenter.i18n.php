ac42eb8316cd76ac48aa7994d2319d7327a14de0
nmlgc <nmlgc@thpatch.net>
2013-10-28 18:27:02 +0100

    Show the list of restricted patch templates on blocked edits.
    ... and all it took was a change to the system message. Nice.

--- TouhouPatchCenter.i18n.php
+++ TouhouPatchCenter.i18n.php
@@ -15,7 +15,7 @@ $messages['en'] = array(
 	'grouppage-patchdev' => '{{ns:project}}:Patch developers',
 	'right-tpc-restricted' => 'Add and modify restricted patch templates',
 	'tpc-desc' => "Creates a server for the ''Touhou Community Reliant Automatic Patcher'' from MediaWiki pages.",
-	'tpc-edit-blocked' => 'You do not have permission to add, remove or modify the content of restricted patch templates.',
+	'tpc-edit-blocked' => "You do not have permission to add, remove or modify the content of restricted patch templates. These are:\n\n{{thcrap_restricted_templates}}\n\nYou can, however, use the corresponding <tt>/test</tt> subtemplates, which are rendered identically, but not parsed for patches.",
 	'tpc-template' => '<code>&#123;&#123;$1&#125;&#125;</code>'
 );
 $magicWords['en'] = array(
@@ -30,5 +30,5 @@ $messages['de'] = array(
 	'grouppage-patchdev' => '{{ns:project}}:Patch-Entwickler',
 	'right-tpc-restricted' => 'Beschränkte Patch-Vorlagen hinzufügen und bearbeiten',
 	'tpc-desc' => "Erstellt einen Server für den ''Touhou Community Reliant Automatic Patcher'' aus MediaWiki-Seiten.",
-	'tpc-edit-blocked' => 'Du bist nicht berechtigt, beschränkte Patch-Vorlagen hinzuzufügen, zu löschen, oder deren Inhalt zu ändern.'
+	'tpc-edit-blocked' => 'Du bist nicht berechtigt, beschränkte Patch-Vorlagen hinzuzufügen, zu löschen, oder deren Inhalt zu ändern. Zu diesen Vorlagen gehören:\n\n$1\n\nDu kannst jedoch die entsprechendenden <tt>/test</tt>-Untervorlagen nutzen. Diese werden identisch dargestellt, aber nicht in Patches einbezogen.'
 );
