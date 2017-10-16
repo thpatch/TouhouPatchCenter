4da085f3ddc7c9aecf596fad3ac9f6ed565ec74d
nmlgc <nmlgc@nmlgc.net>
2017-10-16 21:46:10 +0200

    Tasofro: Replace unsupported bold and italic markup with asterisks.

--- hooks/TPCFmtTasofro.php
+++ hooks/TPCFmtTasofro.php
@@ -15,7 +15,13 @@ class TPCFmtTasofro {
 	public static function onTT( &$tpcState, &$title, &$temp ) {
 		$code = $tpcState->autoCode( $temp );
 
-		$lines = TPCParse::parseLines( $temp->params['tl'], false );
+		// Unsupported by the Tasofro format patchers
+		$tl = $temp->params['tl'];
+		$tl = preg_replace( "/'''''(.*?)'''''/", '*\1*', $tl );
+		$tl = preg_replace( "/'''(.*?)'''/", '*\1*', $tl );
+		$tl = preg_replace( "/''(.*?)''/", '*\1*', $tl );
+
+		$lines = TPCParse::parseLines( $tl, false );
 		// Don't write a JSON null for empty boxes to keep patch stacking functional.
 		if( $lines ) {
 			$tpcState->jsonContents[$code]['lines'] = &$lines;
