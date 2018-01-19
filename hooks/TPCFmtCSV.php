ab0254382013807285d453b36da074503cd96a35
nmlgc <nmlgc@nmlgc.net>
2018-01-19 23:57:42 +0100

    TPCFmtCSV: Prevent the subkeys from being encoded as a JSON array.
    So here we have…:
    • PHP not distinguishing between maps and regular sequential arrays
    • PHP auto-converting string keys for arrays into integers if possible
      (with no way to disable that), and
    • PHP's `json_encode` choosing arrays instead of objects since that
      makes more sense if there is a 0 key and nothing else.
    A proper fix would involve refactoring quite a lot of the parser, so
    here's a workaround for the meantime.

--- hooks/TPCFmtCSV.php
+++ hooks/TPCFmtCSV.php
@@ -11,11 +11,16 @@
   */
 
 class TPCFmtCSV {
+	public static function workAroundZero( &$arr, $id ) {
+		if( $id == 0 ) {
+			$arr[ '_' ] = '_';
+		}
+	}
 
 	public static function onCSV( &$tpcState, &$title, &$temp ) {
 		$id = TPCUtil::dictGet( $temp->params['id'] );
 		$tl = TPCUtil::dictGet( $temp->params['tl'] );
-		if ( empty( $id ) or empty( $tl ) ) {
+		if ( empty( $tl ) ) {
 			return true;
 		}
 		$tl = TPCUtil::sanitize( $tl, false );
@@ -24,9 +29,11 @@ class TPCFmtCSV {
 		$ids = explode( ".", $id );
 		$lastSubkeyIndex = count( $ids ) - 1;
 		for( $i = 0; $i < $lastSubkeyIndex; $i++ ) {
+			self::workAroundZero( $subkey, $ids[ $i ] );
 			$subkey = &$subkey[ $ids[ $i ] ];
 		}
-		$subkey[ $ids[ $lastSubkeyIndex ] = &$tl;
+		self::workAroundZero( $subkey, $ids[ $lastSubkeyIndex ]);
+		$subkey[ $ids[ $lastSubkeyIndex ] ] = &$tl;
 		return true;
 	}
 }
