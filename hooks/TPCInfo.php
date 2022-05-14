f07ecd002b515c96f5fa6ef9f605059c7b0a6dd8
brliron <brliron@hotmail.fr>
2022-05-14 17:02:35 +0200

    Remove thcrap_game_info hook
    That thing have been unused for 8 years, since we moved base_tsa to Github

--- hooks/TPCInfo.php
+++ hooks/TPCInfo.php
@@ -43,38 +43,6 @@ class TPCInfo {
 		}
 		return true;
 	}
-
-	public static function onGameInfo( &$tpcState, $title, $temp ) {
-		$pageTitle = strtolower( $title->getDBKey() );
-		// No dictGet here because this would silently _create_ this parameter
-		// with null as content if it wasn't there before... nice, PHP
-		if( isset( $temp->params['game'] ) ) {
-			$game = $temp->params['game'];
-		} else {
-			$game = $pageTitle;
-		}
-		$cont = &$tpcState->getFile( $game );
-		$cont['game'] = $game;
-
-		foreach ( $temp->params as $key => $val ) {
-			switch ( $key ) {
-				case 'latest':
-					$vars = TPCParse::parseCSV( $val );
-					$cont[$key] = $vars;
-					break;
-				default:
-					if ( !strncasecmp( $key, "format", 6 ) ) {
-						$format = substr( $key, 7 );
-						$cont['formats'][$format] = $val;
-					} else {
-						$cont[$key] = $val;
-					}
-					break;
-			}
-		}
-		return true;
-	}
 }
 
 $wgTPCHooks['thcrap_patch_info'][] = 'TPCInfo::onPatchInfo';
-$wgTPCHooks['thcrap_game_info'][] = 'TPCInfo::onGameInfo';
