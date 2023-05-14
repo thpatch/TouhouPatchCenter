e3fbe394fd710c95e5d7d1d8d06a8b6fe15f55af
Egor <egor@opensrc.club>
2023-05-14 17:29:50 +0300

    add EvalThemeRedirects maintenance script

---
+++ maintenance/evalthemeredirects.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+  * Runs evalTitle on all themedb redirects
+  *
+  * @file
+  * @author Egor
+  */
+
+require_once( dirname( __FILE__ ) . "/../../../maintenance/Maintenance.php" );
+
+class TPCEvalThemeRedirects extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function execute() {
+		foreach ( TouhouThemeDB\Title::REDIRECTS as $k => $v ) {
+			foreach ( Title::makeTitle( NS_THEMEDB, $v )->getSubPages() as $subpage ) {
+				$this->output( $subpage->getPrefixedText() . "\n" );
+				TouhouPatchCenter::evalTitle( $subpage );
+			}
+		}
+	}
+}
+
+$maintClass = 'TPCEvalThemeRedirects';
+require_once( RUN_MAINTENANCE_IF_MAIN );
